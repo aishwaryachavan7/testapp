f2235f8dac544a2e0563a93303ad9f478eb6da02
aishwaryachavan7 <34579190+aishwaryachavan7@users.noreply.github.com>
2018-01-30 11:36:46 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['login.js','base.js'],
+  specs: ['login.js','BasePage.js'],
   capabilities: {
     browserName: 'firefox'
   }
